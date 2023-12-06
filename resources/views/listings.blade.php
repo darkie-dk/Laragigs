a8b3e254cd1db84e8ade0fa2109814a9c0da3f55
darkie-dk <kevendaniel.kd@gmail.com>
2023-12-05 21:05:44 -0300

    Add themes and components

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,17 +1,18 @@
-<h1>
-    {{$heading}}
-</h1>
+@extends('layout')
 
-@if(count($listings) == 0)
-<p>No listings found</p>
-@endif
+@section('content')
+@include('partials._hero')
+@include('partials._search')
 
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+    @if(count($listings) == 0)
+    <p>No listings found</p>
+    @endif
+</div>
 @foreach($listings as $listing)
-<h2>
-   <a href="/listing/{{$listing['id']}}">{{$listing['title']}}
-</h2> </a>
-<p>
-    {{$listing['description']}}
-</p>
+<x-listing-card :listing="$listing" />
 
 @endforeach
+
+@endsection
